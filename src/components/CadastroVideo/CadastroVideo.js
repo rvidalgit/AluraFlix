c72c20118f860ef6eed63df2a00246d928b11353
Rodrigo Vidal <rodrigovidal2006@gmail.com>
2020-08-03 19:45:59 -0300

    Criado componente CadastroVideo e App renomeado para Home

---
+++ src/components/CadastroVideo/CadastroVideo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CadastroVideo = () => {
+    return (
+        <div>TESTE</div>
+    );
+};
+
+export default CadastroVideo;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Home';
 import {BrowserRouter} from 'react-router-dom';
 import {Route, Switch} from 'react-router';
+import CadastroVideo from "./components/CadastroVideo/CadastroVideo";
 
-function CadastroVideo() {
-    return (
-        <div>TESTE</div>
-    );
-}
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/teste" component={CadastroVideo}/>
+            <Route exact path="/" component={Home}/>
+            <Route path="/cadastro/video" component={CadastroVideo}/>
+            <Route component={Pagina404}/>
         </Switch>
     </BrowserRouter>,
     document.getElementById('root')
