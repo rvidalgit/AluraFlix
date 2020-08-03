9afa25c6c9117e2995783ce0da04f2b0790c3926
Rodrigo Vidal <rodrigovidal2006@gmail.com>
2020-08-03 17:59:42 -0300

    Config react-router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {Route, Switch} from 'react-router';
+
+function CadastroVideo() {
+    return (
+        <div>TESTE</div>
+    );
+}
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App}/>
+            <Route path="/teste" component={CadastroVideo}/>
+        </Switch>
+    </BrowserRouter>,
     document.getElementById('root')
 );
